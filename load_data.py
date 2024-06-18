a2f2b0f10bfc60ed0650909c26b5adc2d7ea8e3a
Mami Hayashida <mami.hayashida@uky.edu>
2024-06-18 21:26:46 +0000

    Complete UUID assignment data loading

--- load_data.py
+++ load_data.py
@@ -48,7 +48,7 @@ class GraphDataLoader:
         print(len(records))
 
 
-    def load_EduceLabID(self, uuid_file):
+    def add_EduceLabID(self, uuid_file):
         try:
             records, summary, keys = self.driver.execute_query("""
                 LOAD CSV WITH HEADERS FROM $uuid_f as line
@@ -61,12 +61,62 @@ class GraphDataLoader:
             print(e)
 
 
+    def add_replacement_EduceLabID(self, uuid_file):
+        try:
+            records, summary, keys = self.driver.execute_query("""
+                LOAD CSV WITH HEADERS FROM $uuid_f  as line
+                WITH line
+                WHERE line.Replacement_UUID IS NOT NULL
+                MATCH (orig:EduceLabID{id: line.EduceLab_ID})
+                MATCH (new:EduceLabID{uuid: line.Replacement_UUID})
+                MERGE (orig)<-[:REPLACES]-(new)
+                """, uuid_f=uuid_file,
+                database_="neo4j",
+            )
+        except Exception as e:
+            print(e)
+
+    def add_pherc_nodes(self, uuid_file):
+        try:
+            records, summary, keys = self.driver.execute_query("""
+                LOAD CSV WITH HEADERS FROM $uuid_f  as line
+                WITH line
+                WHERE line.PHerc IS NOT NULL
+                MERGE (p:PHerc {name: line.PHerc})
+                """, uuid_f=uuid_file,
+                database_="neo4j",
+            )
+        except Exception as e:
+            print(e)
+
+    def add_cornice_nodes_and_attach(self, uuid_file):
+        # For now, we will call everything Cornice
+        try:
+            records, summary, keys = self.driver.execute_query("""
+                LOAD CSV WITH HEADERS FROM $uuid_f  as line
+                WITH line
+                WHERE line.Pezzo_Cr IS NOT NULL
+                MATCH (p:PHerc {name: line.PHerc})
+                MATCH (e:EduceLabID {id: line.EduceLab_ID})
+                MERGE (p)-[:HAS]->(:Cornice {name: line.Pezzo_Cr})<-[:ASSIGNED_TO]-(e)
+                """, uuid_f=uuid_file,
+                database_="neo4j",
+            )
+        except Exception as e:
+            print(e)
+
+
 
 if __name__ == "__main__":
 
     loader = GraphDataLoader("neo4j://localhost:7687", config.username, config.password)
-    
+
     loader.verify_conn()
-    loader.load_EduceLabID("file:///MellonUUID_mod.csv")
+
+    #loader.delete_all()
+    loader.add_EduceLabID("file:///MellonUUID_mod.csv")
+    loader.add_replacement_EduceLabID("file:///MellonUUID_mod.csv")
+    loader.add_pherc_nodes("file:///MellonUUID_mod.csv")
+    loader.add_cornice_nodes_and_attach("file:///MellonUUID_mod.csv")
     loader.count_all()
     loader.close()
