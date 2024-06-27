d8bfb62d2206e5b80e373e08702d842fa37b1273
Mami Hayashida <mami.hayashida@uky.edu>
2024-06-27 01:03:44 -0400

    First round of basic queries

--- queries.py
+++ queries.py
@@ -21,10 +21,6 @@ class GraphDataLoader:
             """, 
             database_ = "neo4j",
         )
-        print(records)
-        print(summary)
-        print(keys)
-
 
     def return_all(self):
         records, summary, keys = self.driver.execute_query("""
@@ -33,9 +29,9 @@ class GraphDataLoader:
             """, 
             database_ = "neo4j",
         )
-        print(records)
-        print(summary)
-        print(keys)
+        log.debug(records)
+        log.debug(summary)
+        log.debug(keys)
 
 
     def count_all(self):
@@ -48,31 +44,63 @@ class GraphDataLoader:
         print(len(records))
 
 
+    def get_human_readable_name(self, pherc, cornice=None, pezzo=None):
+
+        corn_n = None
+        pezzo_n = None
+
+        if cornice:
+            records, summary, keys = self.driver.execute_query("""
+                MATCH (ph:PHerc {name: $ph})-[:HAS]->(c:Cornice {name: $cor})
+                RETURN ph.human_name, c.human_name            
+                """, ph=pherc,cor=cornice,
+                database_ = "neo4j",
+            )
+            if records:
+                pherc_n = records[0]["ph.human_name"]
+                corn_n = records[0]["c.human_name"]
+
+        if pezzo:
+            # Currently this is irrelevant since there are no pezzo with "names"
+            records, summary, keys = self.driver.execute_query("""
+                MATCH (ph:PHerc {name: $ph})-[:HAS]->(:Cornice)
+                                                -[:HAS]->(pz:Pezzo {name: $pz})
+                RETURN ph.human_name, pz.human_name            
+                """, ph=pherc, pz=pezzo,
+                database_ = "neo4j",
+            )
+            if records:
+                pherc_n = records[0]["ph.human_name"]
+                pezzo_n = records[0]["pz.human_name"]
+
+        return pherc_n, corn_n, pezzo_n
+
+               
     def find_cornici_pezzi(self, pherc):
         records, summary, keys = self.driver.execute_query("""
-            MATCH (ph:PHerc {human_name: $ph})
+            MATCH (ph:PHerc {name: $ph})
             OPTIONAL MATCH (ph)-[:HAS]-(c:Cornice)
             OPTIONAL MATCH (c)-[:HAS]-(p:Pezzo)
             RETURN c.human_name, p.human_name            
             """, ph=pherc,
             database_ = "neo4j",
         )
-        cornices=[]
+        cornici=[]
         pezzi =[]
         for record in records:
             if record["c.human_name"]:
-                cornices.append(record["c.human_name"])
+                cornici.append(record["c.human_name"])
             if record["p.human_name"]:
                 pezzi.append(record["p.human_name"])
 
-        return cornices, pezzi 
+        return cornici, pezzi 
 
         
     def find_datasets(self, dataset_t, pherc, cornice=None, pezzo=None):
         if cornice:
             records, summary, keys = self.driver.execute_query("""
-                MATCH (ph:PHerc {name: $ph})-[:HAS]-(c:Cornice {name: $cor})
-                MATCH (c)-[*..2]-(e:EduceLabID)
+                MATCH (ph:PHerc {human_name: $ph})-[:HAS]-(c:Cornice {human_name: $cor})
+                MATCH (c)<-[:ASSIGNED_TO]-(e:EduceLabID)
                 MATCH (e)<-[:BELONGS_TO]-(n)
                 WHERE $data_t IN LABELS(n)
                 RETURN n
@@ -88,18 +116,47 @@ class GraphDataLoader:
             return properties
 
         else:
-            pass
-            # pezzo
+            # Pezzo
+            records, summary, keys = self.driver.execute_query("""
+                MATCH (ph:PHerc {human_name: $ph})-[:HAS]->(:Cornice)
+                                        -[:HAS]->(pz:Pezzo {human_name: $pz})
+                MATCH (pz)<-[:ASSIGNED_TO]-(e:EduceLabID)
+                MATCH (e)<-[:BELONGS_TO]-(n)
+                WHERE $data_t IN LABELS(n)
+                RETURN n
+                """, data_t=dataset_t, ph=pherc,pz=pezzo,
+                database_ = "neo4j",
+            )
 
+            properties=[]
+            for record in records:
+                dataset = record[0]
+                properties.append(dataset.items())
+                
+            return properties
+            
 
-        
 if __name__ == "__main__":
 
     loader = GraphDataLoader(config.server_addr, config.username, config.password)
     loader.verify_conn()
-    #records = loader.find_cornice_pezzo("76")
-    
+
+    ### TODO: figure out when to use human-readable names vs. names from UUID
+
+    # Find human readable names
+    ph_n, c_n, p_n = loader.get_human_readable_name("467/2", "Scorza", "467/2")
+    print(ph_n, c_n, p_n)
+
+    # Find cornici and pezzi human-readable names for the given papyrus
+    records = loader.find_cornici_pezzi("76")
+    print(records)
+
     # Choices are "SpectralRaw", "PGSRaw", or "FlatbedScanDataset"
+    # These use human-readable names 
     records = loader.find_datasets("SpectralRaw", "1045", cornice="1")
     print(records)
+
+    records = loader.find_datasets("SpectralRaw", "76", pezzo="Left")
+    print(records)
+
     loader.close()
