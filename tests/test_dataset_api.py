1077f578cdbb6d4861a52a67319bb9660e530abb
Mami Hayashida <mami.hayashida@uky.edu>
2025-10-03 16:06:12 -0400

    Improve comments

--- tests/test_dataset_api.py
+++ tests/test_dataset_api.py
@@ -13,10 +13,10 @@ for record in records:
 print("Cornici:", records[0].data()['cr'])
 print("Pezzi:", records[0].data()['pz'])
 
-# Get all datasets
+# Get all dataset records. (Notice, the newest_completed and properties_only flags.)
 records, summary, keys = db.find_datasets(hercdb.PGSRawType, "1044", cornice="6", newest_completed=False, properties_only=False)
 print(f"\ndataset record: {[record.data() for record in records]}")
 
-# Get datasets properties only
+# Get dataset properties only (Notice, the newest_completed and properties_only flags.)
 properties = db.find_datasets(hercdb.PGSRawType, "1044", cornice="6", newest_completed=False, properties_only=True)
 print(f"\ndataset properties only: {properties}")
